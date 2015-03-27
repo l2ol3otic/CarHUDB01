c20f36f18bc7f9e78737956a044fdbd4723dce48
l2ol3otic <l2ol3otic@gmail.com>
2015-03-27 23:56:25 +0700

    Initial commit

---
+++ app/src/main/java/com/thesis/carhud/carhudb01/Fragment3.java
@@ -0,0 +1,23 @@
+package com.thesis.carhud.carhudb01;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by l2ol3otic2 on 3/25/2015.
+ */
+public class Fragment3 extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater , final ViewGroup Container ,Bundle saveIntanceState){
+        rootview = inflater.inflate(R.layout.fragment3_layout,Container,false);
+        Log.d("Page","3");
+        return rootview;
+    }
+}
